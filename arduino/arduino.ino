4af1b13d236fc4dadcc23636e01fd290bb6bfd80
Rasmus <54621803+abbrasris@users.noreply.github.com>
2020-03-27 15:23:10 +0100

    Test motor

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -2,6 +2,9 @@
 #include "EspMQTTClient.h"
 #include <ArduinoJson.h>
 
+#define MOTOR_DIRECTION_PIN 0
+#define MOTOR_SPEED_PIN 5
+
 EspMQTTClient client(
     WIFI_SSID,
     WIFI_PWD,
@@ -12,10 +15,13 @@ EspMQTTClient client(
     1883
 );
 
+int motorSpeed = 0;
+int motorDirection = 0;
+
 void onConnectionEstablished()
 {
   client.subscribe(MQTT_TOPIC, [] (const String &payload)  {
-    Serial.println(payload);
+    // Serial.println(payload);
 
     // Allocate the JSON document
     StaticJsonDocument<200> doc;
@@ -30,16 +36,28 @@ void onConnectionEstablished()
       return;
     }
 
-    double force = doc["force"];
+    motorSpeed = (int) doc["speed"];
+    Serial.println(motorSpeed);
+    // Serial.println()
   });
 }
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+
+  pinMode(MOTOR_DIRECTION_PIN, OUTPUT);
+  pinMode(MOTOR_SPEED_PIN, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   client.loop();
+  
+  delay(500);
+  digitalWrite(MOTOR_DIRECTION_PIN, 0);
+  analogWrite(MOTOR_SPEED_PIN, 0);
+
+  delay(500);
+  analogWrite(MOTOR_SPEED_PIN, 1024);
 }
